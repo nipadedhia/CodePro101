6ac2e84ce32894fa09e4afbd17910d69f9d41bff
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-10 12:06:55 -0500

    adding comment and ref link

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./Auth";
 
+// creating a private route to be accessed only when the firebase listener verifies that the user is logged in.
+// ref: https://stackoverflow.com/questions/61778154/i-cant-access-a-private-route-using-firebase
+
 const PrivateRoute = ({ component: RouteComponenent, ...rest }) => {
   const { currentUser } = useContext(AuthContext);
 
