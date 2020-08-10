bff0a7aefe4fc86f1c32f452e42bee5e47bddc2b
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-10 11:43:20 -0500

    removed logo

--- client/src/views/signup/index.js
+++ client/src/views/signup/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import SignupComponent from "../../components/Signup";
 import Nav from "../../components/Nav";
 import "./assets/style.scss";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Signup() {
   return (
@@ -10,8 +10,6 @@ function Signup() {
       <div className="container">
         <div className="row">
           <div className="col-md-6 col-sm-12 col-logo-signup">
-            {/* <img src={require("./logo.svg")} alt="" /> */}
-
             {/* <h3>
               <Link className="back-home-link" to={"/"}>
                 <i class="fas fa-long-arrow-alt-left pr-2"></i>Home
