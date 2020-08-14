c7a97f7b2d9d3ada7e1b2eec603ff95cb555d242
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-13 22:31:41 -0500

    modified styling

--- client/src/views/signup/index.js
+++ client/src/views/signup/index.js
@@ -9,14 +9,7 @@ function Signup() {
     <>
       <div className="container">
         <div className="row">
-          <div className="col s12 m6 l3 col-logo-signup">
-            {/* <h3>
-              <Link className="back-home-link" to={"/"}>
-                <i class="fas fa-long-arrow-alt-left pr-2"></i>Home
-              </Link>
-            </h3> */}
-          </div>
-          <div className="col s12 m6 l3 col-logo-signup">
+          <div className="col s12 m12 l8 col-logo-signup">
             <SignupComponent />
           </div>
         </div>
