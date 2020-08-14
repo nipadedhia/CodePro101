ea5aed1658168f9fb10c124fca87a12cbf5a0e4f
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-13 22:31:27 -0500

    modified styling

--- client/src/views/login/index.js
+++ client/src/views/login/index.js
@@ -8,14 +8,14 @@ function Login() {
     <>
       <div className="container">
         <div className="row">
-          <div className="col s12 m6 l3 col-logo-signup">
+          <div className="col s12 m8 l8 col-logo-signup">
             {/* <h3>
               <Link className="back-home-link" to={"/"}>
                 <i class="fas fa-long-arrow-alt-left pr-2"></i>Home
               </Link>
             </h3> */}
           </div>
-          <div className="col s12 m6 l3 col-logo-signup">
+          <div className="col s12 m8 l8 col-logo-signup">
             <LoginComponent />
           </div>
         </div>
