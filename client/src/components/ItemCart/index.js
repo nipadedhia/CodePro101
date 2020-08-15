f3cd9874d84902ba29dafbd009f765c475faf381
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-15 13:31:09 -0500

    made changes in display

--- client/src/components/ItemCart/index.js
+++ client/src/components/ItemCart/index.js
@@ -8,40 +8,40 @@ function ItemCart(props) {
       {props.items.map((item) => (
         <div className="card small blue-grey lighten-2">
           <div className="row ">
-            <div className="col m6 m3">
-              <div className="">
-                {" "}
-                <img
-                  key={item._id}
-                  className="item-img-cart section"
-                  src={item.image}
-                  alt="Course"
-                />
-              </div>
-              <div className="col m10 card-content">
-                <div className="row">
-                  <div className="col s6">
-                    {" "}
-                    <span className="card-title">
-                      {" "}
-                      <h5>{item.name}</h5>
-                    </span>
-                  </div>
-                  <div className="col s6">
-                    {" "}
-                    <h5>${item.price}</h5>
-                  </div>
+            <div className="col s6">
+              <div className=" container">
+                <div className="">
+                  {" "}
+                  <img
+                    key={item._id}
+                    className="item-img-cart section"
+                    src={item.image}
+                    alt="Course"
+                  />
                 </div>
 
-                <div className="row remove-row">
-                  <div className="col  ">
-                    <p
-                      id={item._id}
-                      value={item}
-                      className="waves-effect waves-light btn-small  red darken-3 center-align"
-                      onClick={props.deleteItem}>
-                      <i class="fas fa-minus-circle pr-2"></i> Remove
-                    </p>
+                <div className="col m10 card-content">
+                  <div className="row">
+                    <div className="col s6">
+                      {" "}
+                      <h6>{item.name}</h6>
+                    </div>
+                    <div className="col s6">
+                      {" "}
+                      <p>${item.price}</p>
+                    </div>
+                  </div>
+
+                  <div className="row remove-row">
+                    <div className="col  ">
+                      <p
+                        id={item._id}
+                        value={item}
+                        className="waves-effect waves-light btn-small  red darken-3 center-align"
+                        onClick={props.deleteItem}>
+                        <i class="fas fa-minus-circle pr-2"></i> Remove
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
