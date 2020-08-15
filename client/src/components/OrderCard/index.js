e568da4fa71e516c1fab7b194b418859a78430c8
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-15 13:30:14 -0500

    added display for material cost

--- client/src/components/OrderCard/index.js
+++ client/src/components/OrderCard/index.js
@@ -3,25 +3,35 @@
 // import { useEffect } from "react";
 // import API from "../../Utils/API";
 // import Axios from "axios";
+
 import React from "react";
 
 function OrderCard(props) {
   return (
-    <div className="p-4 card ml-auto order-card" style={{ width: "20rem" }}>
-      <div className="col s12 m12  m20 ">
-        <div className="card small blue-grey lighten-4">
-          <div className=" container">
-            <div className="course-card">
-              <h6 className="card-title section">Price Details</h6>
-              <p className="card-text section">Course Total: ${props.total} </p>
-              <div className="border"></div>
+    <div className="p-4 card ml-auto" style={{ width: "20rem" }}>
+      <div class="">
+        <div className="col s12 m12">
+          <div className="card medium blue-grey lighten-4">
+            <div className=" container">
+              <div className="course-card">
+                <h1 className="card-title section">Price Details</h1>
+                <p className="card-text section">
+                  Course Total: ${props.total}{" "}
+                </p>
+                <p className="card-text section">
+                  Material Cost: ${props.material}
+                </p>
+                <p class="card-text section">
+                  Order Total: ${props.totalPlusMaterial}
+                </p>
 
-              <div>
-                <button
-                  className="waves-effect waves-light btn-large"
-                  onClick={props.checkout}>
-                  Place Order
-                </button>
+                <div>
+                  <button
+                    className="waves-effect waves-light btn-large"
+                    onClick={props.checkout}>
+                    Place Order
+                  </button>
+                </div>
               </div>
             </div>
           </div>
