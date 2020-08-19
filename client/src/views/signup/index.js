4e34585f19e599865c2a887b87c0e72ffbacf4a1
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-08-18 21:02:33 -0500

    removed unwanted code

--- client/src/views/signup/index.js
+++ client/src/views/signup/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SignupComponent from "../../components/Signup";
-import Nav from "../../components/Nav";
+// import Nav from "../../components/Nav";
 import "./assets/style.scss";
 // import { Link } from "react-router-dom";
 
